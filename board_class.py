865ef673e71f60bd33dfc93b335c8fb2325dd294
Kevin C. Escobedo <escobedo001@gmail.com>
2019-05-28 11:15:18 -0700

    Adding handful of methods to Board class

---
+++ board_class.py
@@ -0,0 +1,42 @@
+#Author: Kevin C. Escobedo
+#Email: escobedo001@gmail.com
+
+class Jeopardy_Board:
+    '''Class that represents a board'''
+    def __init__(self, rows = 1, columns = 1):
+        '''Initializes a 2D board'''
+        self.rows = rows
+        self.columns = columns
+        self.board = self.make_board()
+        self.values = {0: 200, 1: 400, 2: 600, 3: 800, 4: 1000}
+
+    def make_board(self) -> [[bool]]:
+        '''Makes a 2D list with each value initialized to 0'''
+        board = []
+        for i in range(self.rows):
+            board.append([])
+            for j in range(self.columns):
+                board[-1].append(0)
+        return board
+
+    def get_score(self, double: bool = False) -> int:
+        '''Calculates the current score'''
+        total = 0
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.board[i][j]:
+                    if double:
+                        total += 2 * self.values[i]
+                    else:
+                        total += self.values[i]
+        return total
+
+    def show_board(self) -> None:
+        '''Shows the current board'''
+        for i in range(self.rows):
+            for j in range(self.columns):
+                print(self.board[i][j], end = ' ')
+            print()
+
+
+
